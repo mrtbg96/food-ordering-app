98792f8745699dee78c14377f07cd83d9dcefc7a
Mert Bağ <mertbag@indivitech.com>
2025-07-16 16:53:37 +0300

    02: Users, Roles and Permissions

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -21,6 +21,19 @@ class AuthServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        $this->registerGates();
+    }
+
+    protected function registerGates(): void
+    {
+        try {
+            foreach (Permission::pluck('name') as $permission) {
+                Gate::define($permission, function ($user) use ($permission) {
+                    return $user->hasPermission($permission);
+                });
+            }
+        } catch (\Exception $e) {
+            info('registerPermissions(): Database not found or not yet migrated. Ignoring user permissions while booting app.');
+        }
     }
 }
